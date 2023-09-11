d9d6d12b0a919c1b1468c270a759a14dffdbcf7a
Mitja Karhusaari <mitja.karhusaari@helsinki.fi>
2023-09-11 21:05:27 +0300

    Update the domain in sent emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.active_storage.service = :local
 
-  config.action_mailer.default_url_options = { host: 'fuksipassi.tko-aly.fi/' }
+  config.action_mailer.default_url_options = { host: 'https://passi.tko-aly.fi' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "haba.tko-aly.fi",
